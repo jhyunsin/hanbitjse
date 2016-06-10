7e2a6e7335a9b6594b8a2672d95c865e31ed756a
hb2009 <hb2009@hb2009-PC>
2016-06-10 09:07:41 +0900

    6.10 오전

---
+++ c/src/branch/num1.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package branch;
+
+import java.util.Scanner;
+
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
+/**
+ * @date   : 2016. 6. 9.
+ * @author : hb2009
+ * @file   : num1.java
+ * @story  : 
+  */
+public class num1 {
+/** 
+ * 저희반은 학생수가 3명입니다
+ * 학생의 평균 점수는 0~100점사이입니다
+ * 3명의 평균점수를 입력하면
+ * 1등 홍길동
+ * 2등 김유신
+ * 3등 김구
+ * 이렇게 나오도록 해주세요 동점은 없습니다
+ * 단, 학생 이름은 스캐너로 받습니다
+ * 1등 김구 96점
+ * 2등 김유신 78점
+ * 3등 홍길동 49점**/
+public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	int score1=0, score2=0, score3=0;
+	String name1, name2, name3;
+	
+	System.out.println("1번학생");
+	name1= scanner.next();
+	System.out.println("2번학생");
+	name2= scanner.next();
+	System.out.println("3번학생");
+	name3= scanner.next();
+	
+	
+			
+			
+	
+	
+	
+	
+	
+}
+	
+	
+
+}
